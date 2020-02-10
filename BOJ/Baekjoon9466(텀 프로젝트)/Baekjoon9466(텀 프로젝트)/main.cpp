6c7a2dd39267868732808389595198373af829a7
KoSangWon <jackoss@naver.com>
2020-02-10 13:54:34 +0900

    200209

--- BOJ/Baekjoon9466(텀 프로젝트)/Baekjoon9466(텀 프로젝트)/main.cpp
+++ BOJ/Baekjoon9466(텀 프로젝트)/Baekjoon9466(텀 프로젝트)/main.cpp
@@ -7,9 +7,51 @@
 //
 
 #include <iostream>
+#include <cstring>
+using namespace std;
+
+int connect[100001];//화살표가 한방향으로 가기때문에 vector대신 배열 사용가능
+bool done[100001];
+bool visited[100001];
+int T, n, cnt;
+
+void dfs(int x){
+    visited[x] = true;
+    int y = connect[x];
+    if(!visited[y])
+        dfs(y);
+    else{
+        if(!done[y]){
+            for(int i = y; i != x; i = connect[i]){//
+                cnt++;//연결된 점들 개수 세기
+            }
+            cnt++;//자기 자신
+        }
+    }
+    done[x] = true;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cin >> T;
+    for(int i = 0; i < T; i++){
+        cin >> n;
+        
+        cnt = 0;
+        memset(connect, 0, sizeof(connect));
+        memset(done, false, sizeof(done));
+        memset(visited, false, sizeof(visited));
+        
+        for(int j = 1; j <= n; j++){
+            cin >> connect[j];
+        }
+        for(int j = 1; j <= n; j++){
+            if(!visited[j])
+                dfs(j);
+        }
+        
+        cout << n - cnt << '\n';
+    }
     return 0;
 }
+
+
