c2b4bee5c76ba18574ac322a41bcaef765e96714
KoSangWon <jackoss@naver.com>
2020-02-11 13:27:31 +0900

    200210

---
+++ BOJ/Baekjoon2667(단지번호붙이기)/Baekjoon2667(단지번호붙이기)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Baekjoon2667(단지번호붙이기)
+//
+//  Created by 고상원 on 2020/02/11.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
