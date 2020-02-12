a34a8a8e01127e36b182564e06b1157eaa410689
KoSangWon <jackoss@naver.com>
2020-02-13 01:17:01 +0900

    200211

--- BOJ/Baekjoon2751(수 정렬하기 2)/Baekjoon2751(수 정렬하기 2)/main.cpp
+++ BOJ/Baekjoon2751(수 정렬하기 2)/Baekjoon2751(수 정렬하기 2)/main.cpp
@@ -7,8 +7,26 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
+    int N;
+    int num;
+    vector<int> arr;
+    
+    cin >> N;
+    
+    for(int i = 0; i < N; i++){
+        cin >> num;
+        arr.push_back(num);
+    }
+    
+    sort(arr.begin(), arr.end());
+    
+    for(int i = 0; i < N; i++)
+        cout << arr[i] << '\n';
     
     return 0;
 }
