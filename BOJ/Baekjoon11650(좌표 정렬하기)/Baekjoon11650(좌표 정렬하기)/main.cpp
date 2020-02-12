08cdb15c4b0ee7d0aba6e7240005bc74f497ac0d
KoSangWon <jackoss@naver.com>
2020-02-12 15:17:18 +0900

    200212

---
+++ BOJ/Baekjoon11650(좌표 정렬하기)/Baekjoon11650(좌표 정렬하기)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Baekjoon11650(좌표 정렬하기)
+//
+//  Created by 고상원 on 2020/02/12.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
