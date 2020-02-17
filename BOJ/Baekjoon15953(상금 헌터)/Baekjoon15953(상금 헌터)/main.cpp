8c2a13c883123d8a862a5ef43600ac27fa9049c9
KoSangWon <jackoss@naver.com>
2020-02-18 03:27:44 +0900

    200217

--- BOJ/Baekjoon15953(상금 헌터)/Baekjoon15953(상금 헌터)/main.cpp
+++ BOJ/Baekjoon15953(상금 헌터)/Baekjoon15953(상금 헌터)/main.cpp
@@ -7,8 +7,51 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
+    int T;
+    int a = 0;
+    int b = 0;
+    int rank = 1;
+    int firstReward[6] = {500, 300, 200, 50, 30, 10};
+    int firstGet = 0;
+    int secondReward[5] = {512, 256, 128, 64, 32};
+    int secondGet = 0;
+    //vector<int> v[8];
+    cin >> T;
+    
+    for(int i = 0; i < T; i++){
+        cin >> a >> b;
+        rank = 1;
+        firstGet = 0;
+        for(int j = 1; j <= 6; j++){
+            for(int k = 1; k <= j; k++){
+                //v[j].push_back(rank);
+                if(a == rank){
+                    firstGet = firstReward[j-1];
+                }
+                rank++;
+            }
+        }
+        
+        rank = 1;
+        secondGet = 0;
+        for(int j = 0; j <= 4; j++){
+            for(int k = 1; k <= pow(2,j); k++){
+                //v[j].push_back(rank);
+                if(b == rank){
+                    secondGet = secondReward[j];
+                }
+                rank++;
+            }
+        }
+        
+        cout << (firstGet + secondGet) * 10000 << '\n';
+    }
+    
     
     return 0;
 }
