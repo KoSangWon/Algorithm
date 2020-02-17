5c459a8cfeb773362d070080adfb43db52e02f7e
KoSangWon <jackoss@naver.com>
2020-02-18 03:27:21 +0900

    200216

--- BOJ/Baekjoon2133(타일 채우기)/Baekjoon2133(타일 채우기)/main.cpp
+++ BOJ/Baekjoon2133(타일 채우기)/Baekjoon2133(타일 채우기)/main.cpp
@@ -7,8 +7,24 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    
+    int N;
+    int dp[31];
+    cin >> N;
+    dp[0] = 1;
+    dp[2] = 3;
+    if(N % 2 == 1)
+        dp[N] = 0;
+    else{
+        for(int i = 4; i <= N; i += 2){
+            dp[i] = 3 * dp[i-2];
+            for(int j = 4; j <= i; j += 2){
+                dp[i] += 2 * dp[i - j];
+            }
+        }
+    }
+    cout << dp[N] << '\n';
     return 0;
 }
