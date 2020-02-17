a802669195cebde0f5d3af12fdae3621c3246bf4
KoSangWon <jackoss@naver.com>
2020-02-17 20:20:20 +0900

    200216

---
+++ BOJ/Baekjoon15953(상금 헌터)/Baekjoon15953(상금 헌터)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Baekjoon15953(상금 헌터)
+//
+//  Created by 고상원 on 2020/02/17.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ BOJ/Baekjoon2133(타일 채우기)/Baekjoon2133(타일 채우기)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Baekjoon2133(타일 채우기)
+//
+//  Created by 고상원 on 2020/02/17.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
