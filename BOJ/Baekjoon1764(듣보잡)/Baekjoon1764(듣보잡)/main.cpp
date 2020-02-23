56487fa7d6dfa19ae6abc58f4d44e425c8922889
KoSangWon <jackoss@naver.com>
2020-02-24 01:24:29 +0900

    200224

--- BOJ/Baekjoon1764(듣보잡)/Baekjoon1764(듣보잡)/main.cpp
+++ BOJ/Baekjoon1764(듣보잡)/Baekjoon1764(듣보잡)/main.cpp
@@ -7,38 +7,52 @@
 //
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<string> v1; //듣도 못한 사람
-vector<string> v2; //보도 못한 사람
-vector<string> v3; //듣도 보도 못한 사람
+int N, M;
+vector<string> v;//듣도 못한 사람
+
+bool binarySearch(int low, int high, string name){
+    if(low > high)
+        return false;
+    else{
+        int mid = (low + high) / 2;
+        if(v[mid] == name)
+            return true;
+        else if(v[mid] > name)
+            return binarySearch(low, mid - 1, name);
+        else
+            return binarySearch(mid + 1, high, name);
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    int N, M;
-    string str;
     cin >> N >> M;
     for(int i = 0; i < N; i++){
-        cin >> str;
-        v1.push_back(str);
+        string name;
+        cin >> name;
+        v.push_back(name);
     }
+    
+    sort(v.begin(), v.end());
+    
+    vector<string> result;
     for(int i = 0; i < M; i++){
-        cin >> str;
-        v2.push_back(str);
+        string name;
+        cin >> name;
+        
+        //듣도 보도 못한 사람
+        if(binarySearch(0, (int)v.size()-1, name))
+            result.push_back(name);
     }
     
-    for(int i = 0; i < v1.size(); i++){
-        for(int j = 0; j < v2.size(); j++){
-            if(v1[i] == v2[j])
-                v3.push_back(v1[i]);
-        }
-    }
+    sort(result.begin(), result.end());
     
-    sort(v3.begin(), v3.end());
-    cout << v3.size() << '\n';
-    for(int i = 0; i < v3.size(); i++)
-        cout << v3[i] << '\n';
+    cout << result.size() << '\n';
+    for(int i = 0; i < result.size(); i++)
+        cout << result[i] << '\n';
     
     return 0;
 }
