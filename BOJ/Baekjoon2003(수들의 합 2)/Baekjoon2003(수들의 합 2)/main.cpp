39dab3233204b0c8f96b4a2db6be4b8a01cb3097
KoSangWon <jackoss@naver.com>
2020-02-29 12:43:23 +0900

    200228

--- BOJ/Baekjoon2003(수들의 합 2)/Baekjoon2003(수들의 합 2)/main.cpp
+++ BOJ/Baekjoon2003(수들의 합 2)/Baekjoon2003(수들의 합 2)/main.cpp
@@ -12,29 +12,21 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     int A[10001] = {0};
     int N, M;
-    int cnt = 0;
-    int startIdx = 1;
-    int sum = 0;
+    int l = 0, h = 0;//low, high
+    int sum = 0, cnt = 0;
     
     cin >> N >> M;
-    for(int i = 1; i <= N; i++)
+    for(int i = 0; i < N; i++)
         cin >> A[i];
     
-    for(int i = 1; i <= N; i++){
-        startIdx = i;
-        sum = 0;
-        while(1){
-            if(startIdx == N+1) break;
-            sum += A[startIdx];
-            if(sum == M){
-                cnt++;
-                break;
-            }
-            else if(sum > M)
-                break;
-            startIdx++;
-        }
+    while(1){
+        if(sum >= M) sum -= A[l++];
+        else if(h == N) break;
+        else sum += A[h++];
+        
+        if(sum == M) cnt++;
     }
+    
     cout << cnt << '\n';
     return 0;
 }
