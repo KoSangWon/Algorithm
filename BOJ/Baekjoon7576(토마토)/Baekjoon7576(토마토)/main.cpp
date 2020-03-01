9f5b66d04060f06a8c121b4d5858b14d5d7ed51a
KoSangWon <jackoss@naver.com>
2020-03-01 18:30:32 +0900

    200301

---
+++ BOJ/Baekjoon7576(토마토)/Baekjoon7576(토마토)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Baekjoon7576(토마토)
+//
+//  Created by 고상원 on 2020/03/01.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
