1149dd0389ae4f6a4264016b8eb5fd21ca40f6f8
KoSangWon <jackoss@naver.com>
2020-03-07 20:55:56 +0900

    200307

--- BOJ/Baekjoon14501(퇴사)/Baekjoon14501(퇴사)/main.cpp
+++ BOJ/Baekjoon14501(퇴사)/Baekjoon14501(퇴사)/main.cpp
@@ -7,18 +7,30 @@
 //
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
+int N;
+int T[16];
+int P[16];
+int dp[16];
+
 int main(int argc, const char * argv[]) {
-    int N, T, P;
-    vector<pair<int, int>> arr;
     cin >> N;
 
     for(int i = 1; i <= N; i++){
-        cin >> T >> P;
-        
+        cin >> T[i] >> P[i];
     }
+    
+    for(int i = N; i >= 1; i--){
+        if(i + T[i] - 1 > N){
+            dp[i] = max(dp[i + 1], 0);
+            continue;
+        }
+        
+        dp[i] = max(dp[i + T[i]] + P[i], dp[i+1]);
+    }
+    
+    cout << dp[1] <<'\n';
     
     return 0;
 }
