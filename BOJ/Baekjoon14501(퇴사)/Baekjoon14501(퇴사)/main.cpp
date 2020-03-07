3c7011fa3d3692de22c18dea3d333e50694e39e6
KoSangWon <jackoss@naver.com>
2020-03-07 20:41:45 +0900

    200307

---
+++ BOJ/Baekjoon14501(퇴사)/Baekjoon14501(퇴사)/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Baekjoon14501(퇴사)
+//
+//  Created by 고상원 on 2020/03/07.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int N, T, P;
+    vector<pair<int, int>> arr;
+    cin >> N;
+
+    for(int i = 1; i <= N; i++){
+        cin >> T >> P;
+        
+    }
+    
+    return 0;
+}
