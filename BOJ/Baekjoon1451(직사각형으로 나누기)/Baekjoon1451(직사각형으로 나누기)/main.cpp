07a9f6a3494842e88abe9805ae89fcd423b8a4cd
KoSangWon <jackoss@naver.com>
2020-03-15 02:31:07 +0900

    200314

---
+++ BOJ/Baekjoon1451(직사각형으로 나누기)/Baekjoon1451(직사각형으로 나누기)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Baekjoon1451(직사각형으로 나누기)
+//
+//  Created by 고상원 on 2020/03/15.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
