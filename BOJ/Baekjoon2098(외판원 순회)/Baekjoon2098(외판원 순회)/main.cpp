23da81a16cdd43639b9d4b5d9de36a4276f7ea0c
KoSangWon <jackoss@naver.com>
2020-03-19 01:41:48 +0900

    200318

--- BOJ/Baekjoon2098(외판원 순회)/Baekjoon2098(외판원 순회)/main.cpp
+++ BOJ/Baekjoon2098(외판원 순회)/Baekjoon2098(외판원 순회)/main.cpp
@@ -1,15 +1,40 @@
-//
-//  main.cpp
-//  Baekjoon2098(외판원 순회)
-//
-//  Created by 고상원 on 2020/03/18.
-//  Copyright © 2020 고상원. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <stdio.h>
+#include <limits.h>
+#include <algorithm>
+using namespace std;
+ 
+int n, all;
+int cost[16][16];
+int dp[16][1 << 16];
+ 
+int func(int cur, int stat) {
+    if (stat == all) {
+        if (cost[cur][0] == 0)
+            return 987654321;
+        else
+            return cost[cur][0];
+    }
+ 
+    int& ref = dp[cur][stat];
+    if (ref != 0)
+        return ref;
+ 
+    int m = INT_MAX - 16000001;
+    for (int i = 1; i < n; ++i) {
+        if (((stat&(1 << i)) == 0) && (cost[cur][i] != 0))
+            m = min(m, func(i, (stat | (1 << i))) + cost[cur][i]);
+    }
+ 
+    return ref = m;
+}
+ 
+int main() {
+    scanf("%d", &n);
+    all = (1 << n) - 1;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            scanf("%d", &cost[i][j]);
+ 
+    printf("%d", func(0, 1));
     return 0;
 }
