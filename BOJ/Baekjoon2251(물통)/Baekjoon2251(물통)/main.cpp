c776e5b4a0d171d1f95a4bca86eb6dbe4aff7c6b
KoSangWon <jackoss@naver.com>
2020-03-21 22:01:49 +0900

    200321

---
+++ BOJ/Baekjoon2251(물통)/Baekjoon2251(물통)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Baekjoon2251(물통)
+//
+//  Created by 고상원 on 2020/03/21.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
