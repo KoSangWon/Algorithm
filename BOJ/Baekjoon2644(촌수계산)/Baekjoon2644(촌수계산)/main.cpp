60e8c6595caf7b9ae45ae80eff03472b103a859a
Sangwon <jackoss@naver.com>
2020-07-04 12:51:14 +0900

    200702

--- BOJ/Baekjoon2644(촌수계산)/Baekjoon2644(촌수계산)/main.cpp
+++ BOJ/Baekjoon2644(촌수계산)/Baekjoon2644(촌수계산)/main.cpp
@@ -6,73 +6,56 @@
 //  Copyright © 2020 고상원. All rights reserved.
 //
 
-
-//DFS, BFS 복습
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <cstring>
+#define MAX 101
 
 using namespace std;
 
-int checked[1001];
-vector<int> a[1001];
+int dist[MAX] = {0};
+vector<int> v[MAX];
 
-void dfs(int x);
-void bfs(int start);
+int bfs(int start, int end);
 
 int main(int argc, const char * argv[]) {
-    int N, M, V;
-    int j, k;
+    int n;
+    int start, end;
+    int m;
+    int x, y;
     
-    cin >> N >> M >> V;
-    for(int i = 0; i < M; i++){
-        cin >> j >> k;
-        a[j].push_back(k);
-        a[k].push_back(j);
+    cin >> n >> start >> end >> m;
+    for(int i = 0; i < m; i++){
+        cin >> x >> y;
+        v[x].push_back(y);
+        v[y].push_back(x);
     }
     
-    for(int i = 1; i <= N; i++){
-        sort(a[i].begin(), a[i].end());
-    }
-    
-    dfs(V);
-    cout << '\n';
-    memset(checked, 0, sizeof(checked));
-    bfs(V);
-    cout << '\n';
+    cout << bfs(start, end) << '\n';
     
     return 0;
 }
 
-
-void dfs(int x){
-    if(checked[x]) return;
-    checked[x] = true;
-    cout << x << ' ';
-    unsigned long size = a[x].size();
-    for(int i = 0; i < size; i++){
-        int y = a[x][i];
-        dfs(y);
-    }
-}
-
-void bfs(int start){
+int bfs(int start, int end){
     queue<int> q;
     q.push(start);
-    checked[start] = true;
+
     while(!q.empty()){
         int x = q.front();
         q.pop();
-        cout << x << ' ';
-        unsigned long size = a[x].size();
+        
+        if(x == end)
+            return dist[end];
+        
+        unsigned long size = v[x].size();
         
         for(int i = 0; i < size; i++){
-            int y = a[x][i];
-            if(!checked[y]){
+            int y = v[x][i];
+            
+            if(!dist[y]){
                 q.push(y);
-                checked[y] = true;
+                dist[y] = dist[x] + 1;
             }
         }
     }
+    return -1;
 }
