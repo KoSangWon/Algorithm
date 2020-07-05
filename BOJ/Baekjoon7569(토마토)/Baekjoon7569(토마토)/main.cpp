2429bb26902257c724b15469ed39965f5b0145b5
Sangwon <jackoss@naver.com>
2020-07-05 19:39:26 +0900

    200705

---
+++ BOJ/Baekjoon7569(토마토)/Baekjoon7569(토마토)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Baekjoon7569(토마토)
+//
+//  Created by 고상원 on 2020/07/05.
+//  Copyright © 2020 고상원. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
